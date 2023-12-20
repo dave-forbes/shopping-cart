5b214b721bf5eda6bf7bf1ad3bda9664fd1c7eaf
David Forbes <davidforbesi@hotmail.com>
2023-12-20 16:16:58 +0000

    added svgs to be used for a logo and for search and shopping cart icons

---
+++ shopping-cart/src/assets/svg/logo.jsx
@@ -0,0 +1,55 @@
+export default function Logo() {
+  return (
+    <svg viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M186.265 59.1083C199.048 48.5133 221.121 43.2531 237.914 50.0361C292.129 71.9328 262.481 145.618 208.149 138.302C171.527 133.373 173.544 65.6624 202.023 54.1592"
+          stroke="#000000"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+        <path
+          d="M157.727 221.885C157.491 224.422 155.334 225.675 152.808 225.918C116.09 229.427 135.537 129.744 164.738 134.296C172.735 135.543 175.95 137.785 179.871 142.576"
+          stroke="#000000"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+        <path
+          d="M211.817 152.757C215.58 170.028 215.939 187.579 215.2 205.18C214.436 223.403 212.309 241.51 210.865 259.676"
+          stroke="#000000"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+        <path
+          d="M161.812 262C161.812 251.866 157.126 214.793 161.812 192.452C164.539 179.453 169.895 164.997 176 153.919"
+          stroke="#000000"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+        <path
+          d="M214.351 353.807C211.661 353.91 206.276 351.956 206.319 349.72C206.423 344.185 206.564 346.386 206.712 340.851C206.959 331.497 207.13 310.06 207.411 301.872C207.547 297.872 206.898 261.489 207.411 257.501C207.912 253.574 207.033 252.748 203.016 252.904C198.17 253.093 181.768 251.053 180.64 250.863C173.283 249.622 175.698 250.389 176.239 257.657C177.101 269.265 173.963 292.526 174.419 304.13C175.03 319.741 172.475 332.781 174.419 349.034C174.826 352.436 176.852 351.86 180.64 352.739"
+          stroke="#000000"
+          strokeOpacity="0.9"
+          strokeWidth="16"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
