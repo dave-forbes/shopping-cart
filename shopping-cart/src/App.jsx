53a7ead38abd59679d4b4a019c1f26be673eda50
David Forbes <davidforbesi@hotmail.com>
2023-12-21 09:51:03 +0000

    changed a tags to react router links

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/hero";
 import Logo from "./assets/svg/logo";
 import Search from "./assets/svg/Search";
 import ShoppingBag from "./assets/svg/ShoppingBag";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
@@ -19,11 +20,11 @@ export default function App() {
       </header>
       <nav>
         <ul>
-          <a>Home</a>
-          <a>Shop all</a>
-          <a>Our story</a>
-          <a>Our Craft</a>
-          <a>Contact</a>
+          <Link to="/">Home</Link>
+          <Link to="/shopall">Shop all</Link>
+          <Link to="/ourstory">Our story</Link>
+          <Link to="/ourcraft">Our Craft</Link>
+          <Link to="/contact">Contact</Link>
         </ul>
       </nav>
       <section className="hero">
