a1c3883598d5926fe6de2f91070849b14a51a4cc
David Forbes <davidforbesi@hotmail.com>
2023-12-21 10:18:09 +0000

    changed button to link to shop section

--- shopping-cart/src/components/hero.jsx
+++ shopping-cart/src/components/hero.jsx
@@ -1,5 +1,14 @@
 import heroImage from "../assets/img/nguyen-le-viet-anh-POcLQMo2COg-unsplash.jpg";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
-  return <img src={heroImage}></img>;
+  return (
+    <section className="hero">
+      <img src={heroImage}></img>
+      <div className="call-to-action">
+        <h2>Shop all bags</h2>
+        <Link to="/shopall">Shop now</Link>
+      </div>
+    </section>
+  );
 }
