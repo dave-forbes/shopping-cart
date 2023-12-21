fa1b062aeb7d672abfed78c0555e1733da3a1335
David Forbes <davidforbesi@hotmail.com>
2023-12-21 10:16:45 +0000

    changed router paths so they are children of the main page

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -3,15 +3,31 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ShopAll from "./pages/ShopAll";
+import OurStory from "./pages/OurStory";
+import Contact from "./pages/Contact";
+import Hero from "./components/hero";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-  },
-  {
-    path: "/shopall",
-    element: <ShopAll />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Hero /> },
+      {
+        path: "/shopall",
+        element: <ShopAll />,
+      },
+      {
+        path: "/ourstory",
+        element: <OurStory />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
