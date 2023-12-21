3a851cff01bcf2727e6241b47cbfb1da97b3a79b
David Forbes <davidforbesi@hotmail.com>
2023-12-21 09:50:34 +0000

    imported react router and setup for routing

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ShopAll from "./pages/ShopAll";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/shopall",
+    element: <ShopAll />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
