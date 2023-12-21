9442f762370e10ed1f37bfddf1bd8169787cba04
David Forbes <davidforbesi@hotmail.com>
2023-12-21 14:48:46 +0000

    added new sign up component

---
+++ shopping-cart/src/components/SignUp.jsx
@@ -0,0 +1,13 @@
+const SignUp = () => {
+  return (
+    <div className="sign-up">
+      <h3>Sign up to our newsletter for updates and offers!</h3>
+      <div>
+        <input />
+        <button>Sign up</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
