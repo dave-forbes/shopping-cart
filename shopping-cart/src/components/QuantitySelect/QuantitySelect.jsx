7051f9cd0a5a24c7b52ca0524e50ff07db4eaa16
David Forbes <davidforbesi@hotmail.com>
2023-12-22 14:48:38 +0000

    changed span to input and added handler for input change

--- shopping-cart/src/components/QuantitySelect/QuantitySelect.jsx
+++ shopping-cart/src/components/QuantitySelect/QuantitySelect.jsx
@@ -3,11 +3,20 @@ import Subtract from "../../assets/svg/Subtract";
 import styles from "./QuantitySelect.module.css";
 import PropTypes from "prop-types";
 
-const QuantitySelect = ({ quantity, clickAdd, clickSubtract }) => {
+const QuantitySelect = ({ quantity, clickAdd, clickSubtract, onChange }) => {
+  const handleInputChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    onChange(newValue);
+  };
+
   return (
     <div className={styles.container}>
       <Subtract onClick={clickSubtract} />
-      <span className={styles.quantity}>{quantity}</span>
+      <input
+        className={styles.quantity}
+        value={quantity}
+        onChange={handleInputChange}
+      />
       <Add onClick={clickAdd} />
     </div>
   );
@@ -17,6 +26,7 @@ QuantitySelect.propTypes = {
   quantity: PropTypes.number,
   clickAdd: PropTypes.func,
   clickSubtract: PropTypes.func,
+  onChange: PropTypes.func,
 };
 
 export default QuantitySelect;
