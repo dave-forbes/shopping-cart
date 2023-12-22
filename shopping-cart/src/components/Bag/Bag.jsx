082615bd039921fe7aa6e84aabd7c7c004f8e578
David Forbes <davidforbesi@hotmail.com>
2023-12-22 12:57:28 +0000

    updated remove from bag, added logic to deal with bag quantity and fixed bug in table layout

--- shopping-cart/src/components/Bag/Bag.jsx
+++ shopping-cart/src/components/Bag/Bag.jsx
@@ -4,17 +4,27 @@ import styles from "./Bag.module.css";
 const Bag = () => {
   const [bag, setBag] = useOutletContext();
 
-  const removeFromBag = () => {
-    setBag("");
+  const removeFromBag = (product) => {
+    const bagCopy = [...bag];
+    const foundProduct = bagCopy.find((item) => item.id === product.id);
+    foundProduct.quantity = foundProduct.quantity - 1;
+    if (foundProduct.quantity === 0) {
+      const index = bagCopy.findIndex((item) => item.id === foundProduct.id);
+      bagCopy.splice(index, 1);
+    }
+    setBag(bagCopy);
   };
 
-  console.log(bag);
+  const calcSubtotal = (price, quantity) => {
+    const total = price * quantity;
+    return total.toFixed(2);
+  };
 
   return (
     <section className={styles.displayBag}>
       <h2 className={styles.h2}>Your Bag ({bag.length})</h2>
       {bag.length === 0 ? (
-        <p>Your bag is empty.</p>
+        <p className={styles.emptyBag}>Your bag is empty.</p>
       ) : (
         <div className={styles.tableContainer}>
           <table className={styles.table}>
@@ -27,33 +37,37 @@ const Bag = () => {
               </tr>
             </thead>
             {bag.map((product) => (
-              <>
-                <td colSpan="5" className={styles.dividerContainer}>
-                  <div className={styles.divider}></div>
-                </td>
-                <tbody key={product.id} className={styles.item}>
-                  <tr>
-                    <td className={styles.itemDesc}>
-                      <img className={styles.img} src={product.imgsrc} />
-                      <span className={styles.productName}>{product.name}</span>
-                    </td>
-                    <th>
-                      <span className={styles.productPrice}>
-                        £{product.price}
-                      </span>
-                    </th>
-                    <th>
-                      <span>1</span>
-                    </th>
-                    <th>
-                      <span>£{product.price}</span>
-                    </th>
-                    <th className={styles.remove}>
-                      <button onClick={() => removeFromBag()}>Remove</button>
-                    </th>
-                  </tr>
-                </tbody>
-              </>
+              <tbody key={product.id} className={styles.item}>
+                <tr>
+                  <td colSpan="5" className={styles.dividerContainer}>
+                    <div className={styles.divider}></div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className={styles.itemDesc}>
+                    <img className={styles.img} src={product.imgsrc} />
+                    <span className={styles.productName}>{product.name}</span>
+                  </td>
+                  <th>
+                    <span className={styles.productPrice}>
+                      £{product.price}
+                    </span>
+                  </th>
+                  <th>
+                    <span>{product.quantity}</span>
+                  </th>
+                  <th>
+                    <span>
+                      £{calcSubtotal(product.price, product.quantity)}
+                    </span>
+                  </th>
+                  <th className={styles.remove}>
+                    <button onClick={() => removeFromBag(product)}>
+                      Remove
+                    </button>
+                  </th>
+                </tr>
+              </tbody>
             ))}
           </table>
         </div>
@@ -61,9 +75,15 @@ const Bag = () => {
       <div className={styles.summary}>
         <p className={styles.total}>
           Order Total: £
-          {bag.reduce((curr, acc) => curr + acc.price, 0).toFixed(2)}
+          {bag
+            .reduce((curr, acc) => curr + acc.price * acc.quantity, 0)
+            .toFixed(2)}
         </p>
-        <button>Checkout</button>
+        <button
+          onClick={() => alert("Thanks for shopping with Backpack and Sack!")}
+        >
+          Checkout
+        </button>
       </div>
     </section>
   );
