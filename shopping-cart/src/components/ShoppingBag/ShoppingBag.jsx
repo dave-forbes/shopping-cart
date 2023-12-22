fe0e759cdd0d381130fcb8d54586e13dd214cee6
David Forbes <davidforbesi@hotmail.com>
2023-12-22 15:17:14 +0000

    added logic so that item counter only appears when bag has at least one item

--- shopping-cart/src/components/ShoppingBag/ShoppingBag.jsx
+++ shopping-cart/src/components/ShoppingBag/ShoppingBag.jsx
@@ -4,13 +4,13 @@ import styles from "./ShoppingBag.module.css";
 import { Link } from "react-router-dom";
 
 export default function ShoppingBag({ bag }) {
+  let bagtotal = bag.reduce((curr, acc) => curr + acc.quantity, 0);
+
   return (
     <Link to="/bag">
       <div className={styles.container}>
         <ShoppingBagSVG className={styles.svg} />
-        <div className={styles.counter}>
-          {bag.reduce((curr, acc) => curr + acc.quantity, 0)}
-        </div>
+        {bagtotal > 0 && <div className={styles.counter}>{bagtotal}</div>}
       </div>
     </Link>
   );
