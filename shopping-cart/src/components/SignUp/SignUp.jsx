8e3dcf3d23459455fd9e3124ce4f9d0f638d32c6
David Forbes <davidforbesi@hotmail.com>
2023-12-25 10:20:50 +0000

    created new css module and copied styles from main css

--- shopping-cart/src/components/SignUp/SignUp.jsx
+++ shopping-cart/src/components/SignUp/SignUp.jsx
@@ -1,9 +1,11 @@
+import styles from "./SignUp.module.css";
+
 const SignUp = () => {
   return (
-    <div className="sign-up">
+    <div className={styles.signUp}>
       <h3>Sign up to our newsletter for updates and offers!</h3>
       <div>
-        <input />
+        <input placeholder="Please enter your email" type="email" />
         <button>Sign up</button>
       </div>
     </div>
