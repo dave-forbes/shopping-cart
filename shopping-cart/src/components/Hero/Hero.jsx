9604eee85baf3fc2d87d27d3e8096132501e4d6f
David Forbes <davidforbesi@hotmail.com>
2023-12-25 11:42:15 +0000

    moved styles over from main css and imported into component

--- shopping-cart/src/components/Hero/Hero.jsx
+++ shopping-cart/src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import heroImage from "../../assets/img/original/nguyen-le-viet-anh-POcLQMo2COg-unsplash.jpg";
 import { Link } from "react-router-dom";
+import styles from "./Hero.module.css";
 
 export default function Hero() {
   return (
-    <section className="hero">
+    <section className={styles.hero}>
       <img src={heroImage}></img>
-      <div className="call-to-action">
+      <div className={styles.callToAction}>
         <h2>Shop all bags</h2>
         <Link to="/shop">Shop now</Link>
       </div>
