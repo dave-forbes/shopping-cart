6478033baa4b8e3cb20412baf950c34ffb26d516
David Forbes <davidforbesi@hotmail.com>
2023-12-25 21:30:28 +0000

    added header opacity change on scroll

--- shopping-cart/src/components/Header/Header.jsx
+++ shopping-cart/src/components/Header/Header.jsx
@@ -3,10 +3,26 @@ import ShoppingBag from "../ShoppingBag/ShoppingBag";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
 const Header = ({ bag }) => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const isScrolling = scrollTop > 0;
+      setScrolling(isScrolling);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${scrolling ? styles.scrolled : ""}`}>
       <div className={styles.headerTitle}>
         <h1>Backpack and Sack</h1>
         <Logo />
