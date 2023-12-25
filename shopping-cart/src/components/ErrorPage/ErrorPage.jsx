b7420cb8cdd4aa664a41dc7eac85d1901d3663dc
David Forbes <davidforbesi@hotmail.com>
2023-12-25 11:41:05 +0000

    moved styles from main css and imported

--- shopping-cart/src/components/ErrorPage/ErrorPage.jsx
+++ shopping-cart/src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 
 export default function ErrorPage() {
   return (
-    <div className="error-page">
+    <div className={styles.errorPage}>
       <h1>Oh no, this route doesn&apos;t exist!</h1>
       <Link to="/">
         You can go back to the home page by clicking here, though!
