fff1ac7a3f9b51a71857de8bcdb77cc1559e10ab
David Forbes <davidforbesi@hotmail.com>
2023-12-28 16:28:24 +0000

    added alts to img and changed links to navlinks so that page scrolls to top

--- shopping-cart/src/components/Categories/Categories.jsx
+++ shopping-cart/src/components/Categories/Categories.jsx
@@ -1,33 +1,33 @@
+import { NavLink } from "react-router-dom";
+import styles from "./Categories.module.css";
 import everydayImage from "../../assets/img/small/omar-roque-z5ncx9p6AvM-unsplash small.jpg";
 import adventureImage from "../../assets/img/small/holly-mandarich-UVyOfX3v0Ls-unsplash small.jpg";
-import { Link } from "react-router-dom";
-import styles from "./Categories.module.css";
 
 const Categories = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <img className={styles.img} src={everydayImage} />
+        <img className={styles.img} src={everydayImage} alt="Everyday" />
         <div className={styles.div}>
-          <Link
+          <NavLink
             className={styles.a}
             to="/shop"
             state={{ category: "everyday" }}
           >
             Shop everyday bags
-          </Link>
+          </NavLink>
         </div>
       </div>
       <div className={styles.wrapper}>
-        <img className={styles.img} src={adventureImage} />
+        <img className={styles.img} src={adventureImage} alt="Adventure" />
         <div className={styles.div}>
-          <Link
+          <NavLink
             className={styles.a}
             to="/shop"
             state={{ category: "adventure" }}
           >
             Shop adventure bags
-          </Link>
+          </NavLink>
         </div>
       </div>
     </div>
