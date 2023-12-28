51ca1351f6de1927e1fd53c83cd93d9239245cb2
David Forbes <davidforbesi@hotmail.com>
2023-12-28 09:33:54 +0000

    added content wrapper and padding to home section

--- shopping-cart/src/components/Home/Home.jsx
+++ shopping-cart/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import Hero from "../Hero/Hero";
 import Categories from "../Categories/Categories";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <section>
+    <section className={styles.home}>
       <Hero />
       <Categories />
     </section>
