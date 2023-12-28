c1ac11605a5a67df64cb3cc1c7ca01f8d65f5533
David Forbes <davidforbesi@hotmail.com>
2023-12-28 14:39:30 +0000

    fix file path issue

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { Outlet } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/footer/Footer";
 import { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 
