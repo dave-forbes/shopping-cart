9f0d74ec55161088191f12b97c4ebe5536773936
David Forbes <davidforbesi@hotmail.com>
2023-12-28 10:38:19 +0000

    added new state and event listener to adjust header content based on screen size

--- shopping-cart/src/components/Header/Header.jsx
+++ shopping-cart/src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import Logo from "../../assets/svg/logo";
 import ShoppingBag from "../ShoppingBag/ShoppingBag";
+import BurgerMenu from "../../assets/svg/BurgerMenu";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 const Header = ({ bag }) => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 900);
+
+  const updateScreenSize = () => {
+    setIsDesktop(window.innerWidth > 900);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreenSize);
+    return () => window.removeEventListener("resize", updateScreenSize);
+  });
+
   return (
     <header className={styles.header}>
       <div className={styles.content}>
@@ -12,29 +25,34 @@ const Header = ({ bag }) => {
           <h1>Backpack and Sack</h1>
           <Logo />
         </div>
-        <nav className={styles.mainNav}>
-          <ul>
-            <NavLink
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              to="/"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              to="/shop"
-            >
-              Shop
-            </NavLink>
-            <NavLink
-              className={({ isActive }) => (isActive ? styles.active : "")}
-              to="/ourstory"
-            >
-              Our story
-            </NavLink>
-          </ul>
-        </nav>
+        {isDesktop ? (
+          <nav className={styles.mainNav}>
+            <ul>
+              <NavLink
+                className={({ isActive }) => (isActive ? styles.active : "")}
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => (isActive ? styles.active : "")}
+                to="/shop"
+              >
+                Shop
+              </NavLink>
+              <NavLink
+                className={({ isActive }) => (isActive ? styles.active : "")}
+                to="/ourstory"
+              >
+                Our story
+              </NavLink>
+            </ul>
+          </nav>
+        ) : (
+          ""
+        )}
         <ShoppingBag bag={bag} />
+        {!isDesktop && <BurgerMenu />}
       </div>
     </header>
   );
