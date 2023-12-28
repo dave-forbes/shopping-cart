20b5b91bdfd9db1d0d0dd5e2c1299d5b01afddca
David Forbes <davidforbesi@hotmail.com>
2023-12-28 09:26:19 +0000

    changed placeholder text for sign up input

--- shopping-cart/src/components/SignUp/SignUp.jsx
+++ shopping-cart/src/components/SignUp/SignUp.jsx
@@ -5,7 +5,7 @@ const SignUp = () => {
     <div className={styles.signUp}>
       <h3>Sign up to our newsletter for updates and offers!</h3>
       <div>
-        <input placeholder="Please enter your email" type="email" />
+        <input placeholder="Email" type="email" />
         <button>Sign up</button>
       </div>
     </div>
