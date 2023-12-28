00536bdf5d7e36a168cbe65987cda979346066a0
David Forbes <davidforbesi@hotmail.com>
2023-12-28 10:15:56 +0000

    added content wrappers to apply max width to content for large screens

--- shopping-cart/src/components/Header/Header.jsx
+++ shopping-cart/src/components/Header/Header.jsx
@@ -7,33 +7,35 @@ import PropTypes from "prop-types";
 const Header = ({ bag }) => {
   return (
     <header className={styles.header}>
-      <div className={styles.headerTitle}>
-        <h1>Backpack and Sack</h1>
-        <Logo />
+      <div className={styles.content}>
+        <div className={styles.headerTitle}>
+          <h1>Backpack and Sack</h1>
+          <Logo />
+        </div>
+        <nav className={styles.mainNav}>
+          <ul>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/shop"
+            >
+              Shop
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              to="/ourstory"
+            >
+              Our story
+            </NavLink>
+          </ul>
+        </nav>
+        <ShoppingBag bag={bag} />
       </div>
-      <nav className={styles.mainNav}>
-        <ul>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/shop"
-          >
-            Shop
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? styles.active : "")}
-            to="/ourstory"
-          >
-            Our story
-          </NavLink>
-        </ul>
-      </nav>
-      <ShoppingBag bag={bag} />
     </header>
   );
 };
--- shopping-cart/src/components/Shop/Shop.jsx
+++ shopping-cart/src/components/Shop/Shop.jsx
@@ -7,30 +7,32 @@ export default function Shop() {
 
   return (
     <section className={styles.shopSection}>
-      <nav className={styles.nav}>
-        <ul>
-          <button
-            className={category === "all" ? styles.focus : ""}
-            onClick={() => setCategory("all")}
-          >
-            All
-          </button>
-          <button
-            className={category === "everyday" ? styles.focus : ""}
-            onClick={() => setCategory("everyday")}
-          >
-            Everyday
-          </button>
-          <button
-            className={category === "adventure" ? styles.focus : ""}
-            onClick={() => setCategory("adventure")}
-          >
-            Adventure
-          </button>
-        </ul>
-      </nav>
-      <div className={styles.inventory}>
-        <ProductList category={category} />
+      <div className={styles.content}>
+        <nav className={styles.nav}>
+          <ul>
+            <button
+              className={category === "all" ? styles.focus : ""}
+              onClick={() => setCategory("all")}
+            >
+              All
+            </button>
+            <button
+              className={category === "everyday" ? styles.focus : ""}
+              onClick={() => setCategory("everyday")}
+            >
+              Everyday
+            </button>
+            <button
+              className={category === "adventure" ? styles.focus : ""}
+              onClick={() => setCategory("adventure")}
+            >
+              Adventure
+            </button>
+          </ul>
+        </nav>
+        <div className={styles.inventory}>
+          <ProductList category={category} />
+        </div>
       </div>
     </section>
   );
